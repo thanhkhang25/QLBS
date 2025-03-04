6d2699d8ec50c23aaf84a7b754bc804f1e8d66bb
thanhkhang25 <146222405+thanhkhang25@users.noreply.github.com>
2025-03-04 09:49:54 +0700

    First commit

---
+++ src/main/java/com/mycompany/qlbs/QLBS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.qlbs;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class QLBS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
